b5c27071ae3064ce9133c4013478910c74d37349
monsur22 <monsurahmedshafiq@gmail.com>
2021-08-24 11:08:44 +0600

    get data by using axios v2

--- src/components/List.js
+++ src/components/List.js
@@ -8,47 +8,48 @@ const List = () => {
         const result = await axios(
         'https://hn.algolia.com/api/v1/search?query=redux',
         );
+        // const result = await axios.get("https://hn.algolia.com/api/v1/search?query=redux")
+        // .then(function(response) {
+        //     console.log(response.data);
+        //     // setData(response.data);
+
+        // })
+        // .catch(function(error) {
+        //   console.log(error);
+        // });
+
 
         setData(result.data);
     });
     return (
         <div>
             <h1>LIst page</h1>
-            {data.hits.map((item) => (
+            {/* {data.hits.map((item) => (
                 <li key={item.objectID}>
                 <a href={item.url}>{item.title}</a>
                 </li>
-            ))}
-            {/* <table class="table">
+            ))} */}
+            <table class="table">
                 <thead>
                     <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
+                {data.hits.map((item) => (
+                      <tr key={item.objectID}>
+                        <td scope="row">#</td>
+                        <td>{item.title}</td>
+                        <td>Action</td>
+                      </tr>
+                ))}
+
+
+
                 </tbody>
-            </table> */}
+            </table>
         </div>
     )
 }
