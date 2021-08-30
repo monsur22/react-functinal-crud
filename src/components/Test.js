b5e48148c043bfefd3c1da4ec0cec32c963a4fff
monsur22 <monsurahmedshafiq@gmail.com>
2021-08-30 20:49:19 +0600

    Use pagination 2 for best

--- src/components/Test.js
+++ src/components/Test.js
@@ -7,23 +7,80 @@ import {
     Route,
     Link
   } from "react-router-dom";
-  import Pagination from './Pagination';
+//   import Pagination from './Pagination';
+import "./pagination.css";
 
 const List = () => {
     const [data, setData] = useState([]);
     let history = useHistory();
+    /// pagination start
+    const [currentPage, setcurrentPage] = useState(1);
+    const [itemsPerPage, setitemsPerPage] = useState(1);
 
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage] = useState(2);
+    const [pageNumberLimit, setpageNumberLimit] = useState(5);
+    const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
+    const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
 
-        // Get current posts
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+    const handleClick = (event) => {
+        setcurrentPage(Number(event.target.id));
+      };
 
-    // Change page
-    const paginate = pageNumber => setCurrentPage(pageNumber);
-    
+      const pages = [];
+      for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+        pages.push(i);
+      }
+
+      const indexOfLastItem = currentPage * itemsPerPage;
+      const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+      const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+      const renderPageNumbers = pages.map((number) => {
+        if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+          return (
+            <li
+              key={number}
+              id={number}
+              onClick={handleClick}
+              className={currentPage == number ? "active" : null}
+            >
+              {number}
+            </li>
+          );
+        } else {
+          return null;
+        }
+      });
+      const handleNextbtn = () => {
+        setcurrentPage(currentPage + 1);
+
+        if (currentPage + 1 > maxPageNumberLimit) {
+          setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+          setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+        }
+      };
+
+      const handlePrevbtn = () => {
+        setcurrentPage(currentPage - 1);
+
+        if ((currentPage - 1) % pageNumberLimit == 0) {
+          setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+          setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+        }
+      };
+
+      let pageIncrementBtn = null;
+      if (pages.length > maxPageNumberLimit) {
+        pageIncrementBtn = <li onClick={handleNextbtn}> &hellip; </li>;
+      }
+
+      let pageDecrementBtn = null;
+      if (minPageNumberLimit >= 1) {
+        pageDecrementBtn = <li onClick={handlePrevbtn}> &hellip; </li>;
+      }
+    //Load more Button
+      const handleLoadMore = () => {
+        setitemsPerPage(itemsPerPage + 5);
+      };
+      /// pagination end
     useEffect(async () => {
         await axios.get("http://127.0.0.1:8000/api/model")
         .then(function(response) {
@@ -71,7 +128,7 @@ const List = () => {
                     </tr>
                 </thead>
                 <tbody>
-                {currentPosts.map((item) => (
+                {currentItems.map((item) => (
                     <tr key={item._id}>
                         <td scope="row">{item.id}</td>
                         <td>{item.firstName}</td>
@@ -92,11 +149,40 @@ const List = () => {
 
                 </tbody>
             </table>
-            <Pagination
+            {/* <Pagination
                 postsPerPage={postsPerPage}
                 totalPosts={data.length}
                 paginate={paginate}
-            />
+            /> */}
+            {/* pagination start form here */}
+            <ul className="pageNumbers">
+                <li>
+                    <button
+                        onClick={handlePrevbtn}
+                        disabled={currentPage == pages[0] ? true : false}
+                    >
+                        Prev
+                    </button>
+                </li>
+                {pageDecrementBtn}
+                {renderPageNumbers}
+                {pageIncrementBtn}
+
+                <li>
+                <button
+                    onClick={handleNextbtn}
+                    disabled={currentPage == pages[pages.length - 1] ? true : false}
+                >
+                    Next
+                </button>
+                </li>
+            </ul>
+            {/* for load more button */}
+            <button onClick={handleLoadMore} className="loadmore">
+                Load More
+            </button>
+            {/* pagination end  here */}
+
         </div>
     )
 }
